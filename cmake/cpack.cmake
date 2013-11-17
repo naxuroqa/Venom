241db2645c3fad02c246572565e5d2a4adacd150
naxuroqa <naxuroqa@gmail.com>
2013-11-17 21:19:55 +0100

    cpack/nsis script improved

--- cmake/cpack.cmake
+++ cmake/cpack.cmake
@@ -17,29 +17,43 @@
 #    along with Venom.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-IF(WIN32 AND NOT UNIX)
-  # There is a bug in NSI that does not handle full unix paths properly. Make
-  # sure there is at least one set of four (4) backslashes.
-  SET(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/icons/48x48/\\\\venom.png")
-  #SET(CPACK_NSIS_INSTALLED_ICON_NAME "bin\\\\Venom.exe")
-  SET(CPACK_NSIS_DISPLAY_NAME "${CPACK_PACKAGE_INSTALL_DIRECTORY} GTK+/Vala GUI for Tox")
-  #SET(CPACK_NSIS_HELP_LINK "http:\\\\\\\\www.my-project-home-page.org")
-  #SET(CPACK_NSIS_URL_INFO_ABOUT "http:\\\\\\\\www.my-personal-home-page.com")
-  #SET(CPACK_NSIS_CONTACT "me@my-personal-home-page.com")
-  SET(CPACK_NSIS_MODIFY_PATH ON)
-ELSE(WIN32 AND NOT UNIX)
-  SET(CPACK_STRIP_FILES "bin/venom")
-  SET(CPACK_SOURCE_STRIP_FILES "")
-ENDIF(WIN32 AND NOT UNIX)
+IF(WIN32)
+  FILE(GLOB WIN32LIBS "${CMAKE_CURRENT_BINARY_DIR}/win32libs/*.dll")
+  SET(CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS ${WIN32LIBS})
+  INCLUDE(InstallRequiredSystemLibraries)
+ENDIF(WIN32)
 
-INCLUDE(InstallRequiredSystemLibraries)
-SET(CPACK_PACKAGE_NAME           "venom")
-SET(CPACK_RESOURCE_FILE_LICENSE  "${CMAKE_SOURCE_DIR}/COPYING")
+# Basic settings
+SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "GTK+/Vala GUI for Tox")
+SET(CPACK_PACKAGE_EXECUTABLES    "venom;Venom")
+SET(CPACK_PACKAGE_INSTALL_DIRECTORY   "Venom")
+SET(CPACK_PACKAGE_NAME           "Venom")
 SET(CPACK_PACKAGE_VERSION_MAJOR  "${VENOM_VERSION_MAJOR}")
 SET(CPACK_PACKAGE_VERSION_MINOR  "${VENOM_VERSION_MINOR}")
 SET(CPACK_PACKAGE_VERSION_PATCH  "${VENOM_VERSION_PATCH}")
+SET(CPACK_SOURCE_IGNORE_FILES /build/;\\\\.gitignore;.*~;\\\\.git;CMakeFiles;Makefile;cmake_install\\\\.cmake)
+SET(CPACK_SOURCE_STRIP_FILES TRUE)
+SET(CPACK_STRIP_FILES TRUE)
+
+# Advanced
+SET(CPACK_RESOURCE_FILE_LICENSE  "${CMAKE_SOURCE_DIR}/COPYING")
 SET(CPACK_PACKAGE_VERSION        "${VENOM_VERSION}")
 
+# nsis
+#TODO set correct path depending on architecture
+#SET(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64")
+# There is a bug in NSI that does not handle full unix paths properly. Make
+# sure there is at least one set of four (4) backslashes.
+#SET(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/misc/\\\\venom.ico")
+#SET(CPACK_NSIS_MUI_UNIICON "${CMAKE_SOURCE_DIR}/misc/\\\\venom.ico")
+SET(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/icons/48x48\\\\venom.png")
+#SET(CPACK_NSIS_INSTALLED_ICON_NAME "")
+SET(CPACK_NSIS_DISPLAY_NAME "Venom")
+#SET(CPACK_NSIS_HELP_LINK "http:\\\\\\\\www.my-project-home-page.org")
+#SET(CPACK_NSIS_URL_INFO_ABOUT "http:\\\\\\\\www.my-personal-home-page.com")
+#SET(CPACK_NSIS_CONTACT "me@my-personal-home-page.com")
+SET(CPACK_NSIS_MUI_FINISHPAGE_RUN "venom.exe")
+
+# .deb
 SET(CPACK_DEBIAN_PACKAGE_DEPENDS "libgtk-3-0 (>= 3.2), libgee-0.8-2 (>= 0.8), libtoxcore (>= 0.0)")
-SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "GTK+/Vala GUI for Tox")
 INCLUDE(CPack)
